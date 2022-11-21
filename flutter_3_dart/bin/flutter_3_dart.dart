76a2d6f10773f038519915068ee4f57052d9af6a
ThanhND-Git <thanhvvt199@gmail.com>
2022-11-21 10:32:05 +0700

    Update flutter_3_dart.dart

--- flutter_3_dart/bin/flutter_3_dart.dart
+++ flutter_3_dart/bin/flutter_3_dart.dart
@@ -1,5 +1,27 @@
-import 'package:flutter_3_dart/flutter_3_dart.dart' as flutter_3_dart;
-
 void main(List<String> arguments) {
-  print('Hello world: ${flutter_3_dart.calculate()}!');
+  print('Hello world: !');
+  int a = 6;
+  String b = '120.34';
+  String c = 'true';
+  double d = 120;
+  String x = a.toString();
+  double y = double.parse(b);
+  bool z = true.toString() == c;
+  int k = d.toInt();
+  String w = d.toString();
+  print("x = $x");
+  print("y = $y");
+  print(z);
+  print("k = $k");
+  print("w = $w");
+  thMap();
+}
+
+void thMap() {
+  final myInfo = {
+    'name': 'Vu Van Thanh',
+    'age': 24,
+    'level': 300,
+  };
+  print(myInfo['name']);
 }
